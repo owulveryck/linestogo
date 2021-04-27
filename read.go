ff0cf15010bb26173e9c8a008c552f4f0456b2aa
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2021-04-27 17:04:54 +0200

    feat: add generation directive

--- read.go
+++ read.go
@@ -4,6 +4,8 @@ import (
 	"io"
 )
 
+//go:generate protoc --go_opt=Mproto/defs.proto=. --go_out=module=github.com/owulveryck/linestogo:. --proto_path=proto defs.proto
+
 // Read from r and fill Page
 func Read(r io.Reader, p *Page) error {
 	return p.readFrom(r)
