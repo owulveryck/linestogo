3ae48484df09a93a6c2895703ec8a234d26a1039
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2021-04-28 10:48:35 +0200

    feat(wip): trivial linear regression

--- example/webserver/main.go
+++ example/webserver/main.go
@@ -5,12 +5,14 @@ import (
 	"context"
 	"encoding/xml"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
 	linestogo "github.com/owulveryck/linesToGo"
+	"gonum.org/v1/gonum/stat"
 )
 
 func main() {
@@ -22,9 +24,34 @@ func main() {
 	defer cancel()
 	http.HandleFunc("/", frontPage)
 	http.HandleFunc("/svg", serveSVG(pageC))
+	http.HandleFunc("/svgcorrected", serveSVGCorrected(pageC))
+	http.HandleFunc("/raw", serveRaw(pageC))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+func serveRaw(pageC <-chan string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Try to get a fresh picture
+		select {
+		case page := <-pageC:
+			f, err := os.Open(page)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			defer f.Close()
+			w.Header().Set("content-type", "application/octet-stream")
+			w.Header().Set("Content-Encoding", "gzip")
+			zr := gzip.NewWriter(w)
+			defer zr.Close()
+			io.Copy(zr, f)
+			return
+		case <-time.After(10 * time.Millisecond):
+		}
+		http.Error(w, "no content", http.StatusOK)
+	}
+
+}
 func serveSVG(pageC <-chan string) http.HandlerFunc {
 	p := &linestogo.Page{}
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -42,6 +69,44 @@ func serveSVG(pageC <-chan string) http.HandlerFunc {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
+		case <-time.After(10 * time.Millisecond):
+		}
+		if p != nil {
+			w.Header().Set("content-type", "image/svg+xml")
+			w.Header().Set("Content-Encoding", "gzip")
+			zr := gzip.NewWriter(w)
+			defer zr.Close()
+			enc := xml.NewEncoder(zr)
+			enc.Indent("", "    ")
+			err := enc.Encode(p)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			enc.Flush()
+			return
+		}
+		http.Error(w, "no content", http.StatusOK)
+	}
+}
+func serveSVGCorrected(pageC <-chan string) http.HandlerFunc {
+	p := &linestogo.Page{}
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Try to get a fresh picture
+		select {
+		case page := <-pageC:
+			f, err := os.Open(page)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			defer f.Close()
+			err = linestogo.Read(f, p)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			linearize(p)
 		case <-time.After(10 * time.Millisecond):
 		}
 		if p != nil {
@@ -100,3 +165,25 @@ const skeleton = `
 </body>
 </html>
 `
+
+func linearize(p *linestogo.Page) {
+	for _, l := range p.Layers {
+		for _, s := range l.Strokes {
+			x := make([]float64, len(s.Segments))
+			y := make([]float64, len(s.Segments))
+			for i := 0; i < len(s.Segments); i++ {
+				x[i] = float64(s.Segments[i].X)
+				y[i] = float64(s.Segments[i].Y)
+			}
+			alpha, beta := stat.LinearRegression(x, y, nil, false)
+			// replace the segments
+			s.Segments[len(s.Segments)-1] = &linestogo.Segment{
+				X: s.Segments[len(s.Segments)-1].X,
+				Y: float32(beta)*s.Segments[len(s.Segments)-1].X + float32(alpha),
+			}
+			s.Segments[1] = s.Segments[len(s.Segments)-1]
+			s.Segments = s.Segments[:2]
+		}
+
+	}
+}
--- go.mod
+++ go.mod
@@ -3,10 +3,12 @@ module github.com/owulveryck/linesToGo
 go 1.15
 
 require (
+	github.com/davecgh/go-spew v1.1.0
 	github.com/fsnotify/fsnotify v1.4.9
 	github.com/kr/pretty v0.2.1
 	golang.org/x/image v0.0.0-20210220032944-ac19c3e999fb
 	golang.org/x/sys v0.0.0-20210426230700-d19ff857e887 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gonum.org/v1/gonum v0.9.1
 	google.golang.org/protobuf v1.26.0
 )
