10b14eab8cb30fb9046686e8c88fe0d1c61006c7
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2021-04-28 09:45:28 +0200

    feat: add a couple of CSS for aspect ratio

--- example/webserver/main.go
+++ example/webserver/main.go
@@ -74,11 +74,28 @@ const skeleton = `
 <title>reMarkable</title>
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <style>
+.fullscreen-map {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 110vh;
+    min-width: 1020px;
+    object-fit: cover;
+    object-position: 0;
+    font-family: 'object-fit: cover;';
+    z-index: -1;
+	transform: rotate(90deg);
+}
+
 </style>
 <script src="" height="100%"></script>
 <body>
+<div class="map">
+  <object class="fullscreen-map" type="image/svg+xml" data="/svg">
+	<img src="/svg" >
+  </object>
+</div>
 
-<img src="/svg" >
 
 </body>
 </html>
